32b68156eafc54d1f4187ba65ff45616d22dc48d
Klon3614 <stepanboksgorn7053@gmail.com>
2024-07-25 08:41:27 +0500

    Make half-header component

--- src/pages/Introduce.jsx
+++ src/pages/Introduce.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import HalfHeader from "../components/header/HalfHeader";
 
 export default function Introduce(){
     return (
         <div>
+            <HalfHeader/>
             <h1>Introduce</h1>
         </div>
     );
