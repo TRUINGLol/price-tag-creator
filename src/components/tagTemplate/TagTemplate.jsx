caf48ec408dcff1254c4b8fc7d2264e915b937ea
Klon3614 <stepanboksgorn7053@gmail.com>
2024-08-01 21:41:28 +0500

    create tagTemplate component without API request

---
+++ src/components/tagTemplate/TagTemplate.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cl from "../styles/TagTemplate.module.css";
+
+export default function TagTemplate({width='380px', height='228px'}){
+    return (
+        <div className={cl.TT} style={{width:width, height:height}}>
+            
+        </div>
+    );
+}
