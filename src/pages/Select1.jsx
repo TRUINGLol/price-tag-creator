9bd9a336e6ff472de96c06be0998c47d1e054cbe
Klon3614 <stepanboksgorn7053@gmail.com>
2024-08-01 20:40:02 +0500

    create Select1 template

---
+++ src/pages/Select1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cl from "./styles/Select1.module.css";
+
+export default function Select1(){
+    return (
+        <div className={cl.select}>
+            Select1
+        </div>
+    );
+}
