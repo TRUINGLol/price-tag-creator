dd9e26c0b175ab2543ef4f27f9afb6d10909f8a4
Klon3614 <stepanboksgorn7053@gmail.com>
2024-08-06 22:35:25 +0500

    useEffect template

--- src/pages/MoreTags.jsx
+++ src/pages/MoreTags.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import cl from "./styles/MoreTags.module.css";
 import Header from "../components/header/Header.jsx";
+import Fetch from "../API/Fetch.js";
 
 export default function MoreTags(){
     
+    const[data,setData] = useState([]);
+    const[isLoading, setIsLoading] = useState(false);
+
+    useEffect(()=>{
+        
+    },[]);
+
     return (
         <div className={cl.moreTags}>
             <Header/>
             
+
         </div>
     );
 }
