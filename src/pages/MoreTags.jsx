43f830beadc08ac92c0b472ee2c91043202dd898
Klon3614 <stepanboksgorn7053@gmail.com>
2024-08-06 22:28:56 +0500

    Start  making MoreTags page

--- src/pages/MoreTags.jsx
+++ src/pages/MoreTags.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import cl from "./styles/MoreTags.module.css";
+import Header from "../components/header/Header.jsx";
 
 export default function MoreTags(){
+    
     return (
-        <div></div>
+        <div className={cl.moreTags}>
+            <Header/>
+            
+        </div>
     );
 }
