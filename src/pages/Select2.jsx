5a8c4e22943240a68e0a4d0480fc9ba35590b384
Klon3614 <stepanboksgorn7053@gmail.com>
2024-08-15 23:50:43 +0500

    start making select2 page style and fix header bug

--- src/pages/Select2.jsx
+++ src/pages/Select2.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import cl from "./styles/Select2.module.css";
+import Header from "../components/header/Header";
 
 export default function Select2(){
+    
+    useEffect(()=>{
+        document.body.style.backgroundColor = "#ED8347";
+    },[]);
+    
     return (
-        <div></div>
+        <div>
+            <Header/>
+
+            <div className={cl.mainBlock}>
+
+            </div>
+        </div>
     );
 }
