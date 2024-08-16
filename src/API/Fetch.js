04e5b65d8b4e09afe52e561f68b19e7b45ad33ed
Klon3614 <stepanboksgorn7053@gmail.com>
2024-08-16 20:48:33 +0500

    rewrite fetch tags by count request to prommise catch

--- src/API/Fetch.js
+++ src/API/Fetch.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export default class Fetch{
     static async fetchTagsByCount(setData,setIsLoading,count){
-        try{
-            setIsLoading(true);
-            const responce = await axios.get(`https://localhost:7056/api/tag?count=${count}`);
-            setData(responce.data);        
+        setIsLoading(true);
+        await axios.get(`https://localhost:7056/api/tag?count=${count}`)
+        .then((responce)=>{
             setIsLoading(false);
-        }
-        catch(e){
+            setData(responce.data)
+        })
+        .catch((e)=>{
+            console.log(e.message);
             setIsLoading(true);
-            console.error("Fetch error:", e.message)
-        }
+        });
     }
 
     static async fetchAllTags(setData,setIsLoading){
